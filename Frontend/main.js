13921c94ec700157380e4f6c4dc0d51c6b4cfad9
DarwinDev06 <darwinnoel15@gmail.com>
2023-07-16 13:58:08 -0500

    burgerMenu integrate in mobile version and add responsive design menus

--- Frontend/main.js
+++ Frontend/main.js
@@ -3,10 +3,18 @@
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.menu-desktop')
 
+const burgerMenu = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
+burgerMenu.addEventListener('click', toggleMobileMenu)
 
 function toggleDesktopMenu(){
-    console.log("inactive")
+
     desktopMenu.classList.toggle('inactive')
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive')
 }
